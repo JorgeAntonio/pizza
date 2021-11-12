075cc10d2e18db3246d4b9965b15bebecedc295b
JorgeAntonio <56671752+JorgeAntonio@users.noreply.github.com>
2021-11-12 06:50:57 -0500

    delete and edit feature added

---
+++ resources/views/pizza/edit.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+
+            <div class="col-md-8">
+                @if(count($errors)>0)
+                    <div class="card mt-5">
+                        <div class="card-body">
+                            <div class="alert alert-danger" role="alert">
+                                @foreach($errors->all() as $error)
+                                    <p>{{ $error }}</p>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endif
+
+                <div class="card">
+                    <div class="card-header">Edit Pizza</div>
+                    <form action="{{route('pizza.update',$pizza->id)}}" method="post" enctype="multipart/form-data">
+                        @csrf
+                        @method('PUT')
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="name">Name of pizza</label>
+                                <input type="text" name="name" id="name" class="form-control" value="{{$pizza->name}}">
+                            </div>
+                            <div class="form-group">
+                                <label for="description">Description of pizza</label>
+                                <textarea name="description" id="description" class="form-control">{{$pizza->description}}</textarea>
+                            </div>
+                            <div class="form-inline">
+                                <div class="form-group">
+                                    <label for="price">Pizza price</label>
+                                    <input type="text" name="small_pizza_price" id="price" class="form-control"
+                                           placeholder="small pizza price" value="{{$pizza->small_pizza_price}}">
+                                    <input type="text" name="medium_pizza_price" id="price" class="form-control"
+                                           placeholder="medium pizza price" value="{{$pizza->medium_pizza_price}}">
+                                    <input type="text" name="large_pizza_price" id="price" class="form-control"
+                                           placeholder="large pizza price" value="{{$pizza->large_pizza_price}}">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="category">Category</label>
+                                <select name="category" id="category" class="form-control">
+                                    <option value=""></option>
+                                    <option value="vegetanarian">Vegetanarian Pizza</option>
+                                    <option value="nonvegetarian">Non Vegetarian Pizza</option>
+                                    <option value="traditional">Traditional Pizza</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="image">Image</label>
+                                <input type="file" name="image" id="image" class="">
+                                <img src="{{\Illuminate\Support\Facades\Storage::url($pizza->image)}}" alt="" width="80">
+                            </div>
+                            <div class="form-group text-center">
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -5,8 +5,11 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">All Pizzas</div>
-
+                    <div class="card-header">All Pizzas
+                        <a href="{{route('pizza.create')}}">
+                            <button type="button" class="btn btn-success" style="float: right">Add pizza</button>
+                        </a>
+                    </div>
                     <div class="card-body">
                         @if (session('message'))
                             <div class="alert alert-success" role="alert">
@@ -29,6 +32,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
+                                @if(count($pizzas)>0)
                                 @foreach($pizzas as $key => $pizza)
                                 <tr>
                                     <td scope="row">{{$key+1}}</td>
@@ -39,14 +43,48 @@
                                     <td>{{$pizza->smal_pizza_price}}</td>
                                     <td>{{$pizza->mediun_pizza_price}}</td>
                                     <td>{{$pizza->large_pizza_price}}</td>
-                                    <td><button class="btn btn-primary">Edit</button></td>
-                                    <td><button class="btn btn-danger">Delete</button></td>
+
+                                    <td><a href="{{route('pizza.edit',$pizza->id)}}"><button class="btn btn-primary">Edit</button></a></td>
+                                    <td><button class="btn btn-danger" data-toggle="modal" data-target="#modelId{{$pizza->id}}">Delete</button></td>
+
+                                    <!-- Modal -->
+                                    <div class="modal fade" id="modelId{{$pizza->id}}" tabindex="-1" role="dialog"
+                                         aria-labelledby="modelTitleId" aria-hidden="true">
+                                        <form action="{{route('pizza.destroy',$pizza->id)}}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title">Delete Confirmation</h5>
+                                                    <button type="button" class="close" data-dismiss="modal"
+                                                            aria-label="Close">
+                                                        <span aria-hidden="true">&times;</span>
+                                                    </button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    Are you sure?
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                            data-dismiss="modal">Close
+                                                    </button>
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        </form>
+                                    </div>
+
                                 </tr>
                                 @endforeach
+                                @else
+                                <p>No pizzas to show</p>
+                                @endif
                                 </tbody>
                             </table>
+                        {{ $pizzas->links() }}
                     </div>
-
                 </div>
             </div>
         </div>
