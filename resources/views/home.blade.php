db5f083b69f3a315b84f6b14d6cca11bff75e2b4
JorgeAntonio <56671752+JorgeAntonio@users.noreply.github.com>
2021-11-29 11:29:40 -0500

    frontpage fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">Your Orders</div>
+                    <header class="card-header">Your Orders</header>
                     <div class="card-body">
                         <table class="table table-bordered">
                             <thead class="text-center">
